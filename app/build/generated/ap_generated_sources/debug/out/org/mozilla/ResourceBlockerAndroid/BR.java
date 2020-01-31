c6f2e49cf47330d10ed50a8fad6bae4a4592fda7
Marc Leclair <marc.leclair0113@gmail.com>
2020-01-31 18:03:39 -0500

    .gitignore is now working

--- app/build/generated/ap_generated_sources/debug/out/org/mozilla/ResourceBlockerAndroid/BR.java
+++
@@ -1,5 +0,0 @@
-package org.mozilla.ResourceBlockerAndroid;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/source/dataBinding/trigger/debug/org/mozilla/ResourceBlockerAndroid/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package org.mozilla.ResourceBlockerAndroid;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
